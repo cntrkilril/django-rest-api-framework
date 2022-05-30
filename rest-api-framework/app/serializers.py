64241709ef1473a9c182c52797782fc46e92eea2
cntrkilril <shalkir2002@mail.ru>
2022-05-30 12:46:03 +0300

    add validation in PostSerializer

--- rest-api-framework/app/serializers.py
+++ rest-api-framework/app/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from .models import Post, Profile, Groups
 
@@ -7,10 +8,19 @@ class PostSerializer(ModelSerializer):
         model = Post
         fields = '__all__'
 
+    def validate(self, attrs):
+        likes = attrs['likes']
+        dislikes = attrs['dislikes']
+        if likes != 0 or dislikes != 0:
+            raise serializers.ValidationError({'likes': "Кол-во лайков не может быть больше 0 при создании",
+                                               'dislikes': "Кол-во лайков не может быть больше 0 при создании"})
+
+
 class ProfileSerializer(ModelSerializer):
     class Meta:
         model = Profile
         fields = '__all__'
+
 
 class GroupsSerializer(ModelSerializer):
     class Meta:
